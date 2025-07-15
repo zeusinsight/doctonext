ba9411d637b9c9f7a2aa5844d8aa20a1bef7b011
indieceo <alaa.sbikhi01@gmail.com>
2025-07-15 09:19:13 +0100

    Adding Seline Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import { Providers } from "./providers"
 import type { ReactNode } from "react"
-import { Analytics } from "@vercel/analytics/next"
 import "@/styles/globals.css"
-
-
 
 export default function RootLayout({
     children
@@ -12,11 +9,15 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" suppressHydrationWarning>
+            <head>
+                <script
+                    async
+                    src="/seline.js"
+                    data-token="24cc7b65ecf3469"
+                />
+            </head>
             <body className="flex min-h-svh flex-col antialiased">
-            <Analytics />
-                <Providers>
-                    {children}
-                </Providers>
+                <Providers>{children}</Providers>
             </body>
         </html>
     )
